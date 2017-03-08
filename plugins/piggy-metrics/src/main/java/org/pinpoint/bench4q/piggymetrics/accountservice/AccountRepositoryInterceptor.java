ed1322c0904b9e629ca0aa1f2f678da36bbaa03a
wzy <wziyong@126.com>
2017-03-08 11:40:33 +0800

    piggy metrics pinpoint

---
+++ plugins/piggy-metrics/src/main/java/org/pinpoint/bench4q/piggymetrics/accountservice/AccountRepositoryInterceptor.java
@@ -0,0 +1,32 @@
+package org.pinpoint.bench4q.piggymetrics.accountservice;
+
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentClass;
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentException;
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentMethod;
+import com.navercorp.pinpoint.bootstrap.instrument.Instrumentor;
+import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformCallback;
+import org.pinpoint.bench4q.sample.PiggyMetricsConstants;
+
+import java.security.ProtectionDomain;
+
+import static com.navercorp.pinpoint.common.util.VarArgs.va;
+
+public class AccountRepositoryInterceptor implements TransformCallback {
+
+	@Override
+	public byte[] doInTransform(Instrumentor instrumentor, ClassLoader classLoader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+		// 1. Get InstrumentClass of the target class
+		InstrumentClass target = instrumentor.getInstrumentClass(classLoader, className, classfileBuffer);
+
+		// 2. Get InstrumentMethod of the target method.
+		InstrumentMethod targetMethod = target.getDeclaredMethod("createNewAccount", "com.piggymetrics.account.domain.User");
+
+		// 3. Add interceptor. The first argument is FQN of the interceptor
+		// class, followed by arguments for the interceptor's constructor.
+		targetMethod.addInterceptor("com.navercorp.pinpoint.bootstrap.interceptor.BasicMethodInterceptor", va(PiggyMetricsConstants.MY_SERVICE_TYPE));
+
+		// 4. Return resulting byte code.
+		return target.toBytecode();
+	}
+
+}
